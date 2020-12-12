4a9c44446423030b21fe6b2b51a1120e53946ee3
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-12-12 20:17:56 -0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Raphael Cerqueira on 12/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/DigitalWalletAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalWalletAppApp.swift
+//  Shared
+//
+//  Created by Raphael Cerqueira on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct DigitalWalletAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
